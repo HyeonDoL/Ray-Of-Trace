3e37150a3bddff4334892577b77395787ce02ee0
BlackRoach <black4roach@gmail.com>
2017-05-23 23:09:35 +0900

    좀 시발

---
+++ RayOfTrace/Assets/Script/What.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class What : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// 아 섹스하고싶다
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
