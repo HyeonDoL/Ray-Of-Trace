9be6e67342a83c1aee7edc4a61dcba3968581e1a
BlackRoach <black4roach@gmail.com>
2017-05-25 08:37:29 +0900

    mine

--- RayOfTrace/Assets/Script/UI.cs
+++ RayOfTrace/Assets/Script/UI.cs
@@ -1,16 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 using UniRx;
 public class UI : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+   
 }
