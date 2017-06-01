87714151a42ef1f283adc522f8ef595ee4b90484
BlackRoach <black4roach@gmail.com>
2017-06-01 18:14:52 +0900

    UIstruct 수정

---
+++ RayOfTrace/Assets/Script/Manager/ChapterManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChapterManager : MonoBehaviour {
+
+    [SerializeField]
+    private NormalLight glowLight;
+    [SerializeField]
+    private RepeatLight dimLight;
+    [SerializeField]
+    private RepeatLight repeatLight;
+    [SerializeField] private GameObject ChapterWindow;
+    [SerializeField] private GameObject OptionWindow;
+    [SerializeField] private GameObject ShopWindow;
+
+
+    private IEnumerator ChangeWindow()
+    {
+        repeatLight.gameObject.SetActive(false);
+
+        dimLight.LightIntensity = repeatLight.LightIntensity;
+        yield return StartCoroutine(glowLight.Lighting());
+
+       
+
+     
+    }
+    public void OptionButton()
+    {
+        ChangeWindow();
+        ChapterWindow.SetActive(false);
+        OptionWindow.SetActive(true);
+    }
+    public void ShopButton()
+    {
+        ChangeWindow();
+        ChapterWindow.SetActive(false);
+        ShopWindow.SetActive(true);
+    }
+    public void BackButton()
+    {
+        ChangeWindow();
+        OptionWindow.SetActive(false);
+        ShopWindow.SetActive(false);
+        ChapterWindow.SetActive(true);
+       
+    }
+}
--- RayOfTrace/Assets/Script/Manager/TitleManager.cs
+++ RayOfTrace/Assets/Script/Manager/TitleManager.cs
@@ -9,6 +9,12 @@ public class TitleManager : MonoBehaviour
     [SerializeField]
     private RepeatLight repeatLight;
 
+    [SerializeField]
+    private GameObject ChapterWindow;
+    [SerializeField]
+    private GameObject TitleWindow;
+    [SerializeField]
+    private GameObject BackGround;
     private void Awake()
     {
         StartCoroutine(repeatLight.Lighting());
@@ -27,7 +33,13 @@ public class TitleManager : MonoBehaviour
         normalLight.LightIntensity = repeatLight.LightIntensity;
 
        yield return StartCoroutine(normalLight.Lighting());
+        
+        TitleWindow.SetActive(false);
+        repeatLight.gameObject.SetActive(true);
+        ChapterWindow.SetActive(true);
+        BackGround.SetActive(true);
+        this.gameObject.GetComponent<TitleManager>().enabled= false;
 
-        SceneChange.Change(SceneType.InGame);
+       // SceneChange.Change(SceneType.InGame);
     }
 }
