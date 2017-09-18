642b0900679832d0852489b29f91907621795ea5
HyunDo_Lee <godd7776@gmail.com>
2017-09-19 03:21:41 +0900

    Create StageSheet, AchievementSheet, Achievement Script

---
+++ RayOfTrace/Assets/ScriptableObject/Sheet/AchievementSheet.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[System.Serializable]
+public class AchievementData
+{
+    public string name;
+
+    [TextArea]
+    public string contents;
+
+    
+}
+
+public class AchievementSheet : Sheet<AchievementData>
+{
+#if UNITY_EDITOR
+    [MenuItem("DataSheet/AchievementSheet")]
+    public static void CreateData()
+    {
+        ScriptableObjectUtillity.CreateAsset<AchievementSheet>();
+    }
+#endif
+}
---
+++ RayOfTrace/Assets/ScriptableObject/Sheet/StageSheet.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[System.Serializable]
+public class StageData
+{
+    public int stageLv;
+
+    public List<Achievement> stageAchievementList = new List<Achievement>();
+}
+
+public class StageSheet : Sheet<StageData>
+{
+#if UNITY_EDITOR
+    [MenuItem("DataSheet/StageSheet")]
+    public static void CreateData()
+    {
+        ScriptableObjectUtillity.CreateAsset<StageSheet>();
+    }
+#endif
+}
