d6b0d03e7fe96e71c3b4bd3e448213e1c48b62e4
HyunDo_Lee <godd7776@gmail.com>
2017-09-23 05:37:46 +0900

    Create AchievementTextSheet

---
+++ RayOfTrace/Assets/ScriptableObject/Sheet/AchievementTextSheet.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[System.Serializable]
+public struct AchievementText
+{
+    public string name;
+
+    [TextArea]
+    public string content;
+}
+
+[System.Serializable]
+public class AchievementTextData
+{
+    public List<AchievementText> achievementTextList;
+}
+
+public class AchievementTextSheet : Sheet<AchievementTextData>
+{
+#if UNITY_EDITOR
+    [MenuItem("DataSheet/AchievementTextSheet")]
+    public static void CreateData()
+    {
+        ScriptableObjectUtillity.CreateAsset<AchievementTextSheet>();
+    }
+#endif
+}
